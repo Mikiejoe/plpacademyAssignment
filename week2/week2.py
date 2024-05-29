8447518daa35466107b1547e46ca3c880944ad8b
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-05-29 08:59:57 +0300

    week 2 assignment

---
+++ week2/week2.py
@@ -0,0 +1,14 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.remove(my_list[-1])
+my_list.sort()
+
+index_30 = my_list.index(30)
+print(my_list)
+print(index_30)
